e37a545f3dbdbe8108c85181ce0e92901e0d5352
qmul-wyt <48768919+qmul-wyt@users.noreply.github.com>
2019-03-28 17:26:53 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from cassandra.cluster import Cluster
 
 cluster = Cluster(['cassandra'])
@@ -10,14 +10,26 @@ def hello():
     name = request.args.get("name","World")
     return('<h1>Hello, {}!</h1>'.format(name))
 
-@app.route('/rates/<date>')
-def profile(date):
+@app.route('/rates/<date>/<currency>')
+def profile(date,currency):
     rows = session.execute( """Select * From rates.stats where date = '{}'""".format(date))
-
-    for rates in rows:
-        return('<h1>The rates in {} is {}!</h1>'.format(date,rates.eur))
-
+    if currency == 'cad':
+        for rates in rows:
+            #return('<h1>The exchange rates between GBP and CAD in {} is {}!</h1>'.format(date,rates.cad))
+            return jsonify(rates)
+    elif currency == 'usd':
+        for rates in rows:
+            return('<h1>The exchange rates between GBP and USD in {} is {}!</h1>'.format(date,rates.usd))
+            return jsonify(rates)
+    elif currency == 'cny':
+        for rates in rows:
+            return('<h1>The exchange rates between GBP and CNY in {} is {}!</h1>'.format(date,rates.cny))
+            return jsonify(rates)
+    elif currency == 'eur':
+        for rates in rows:
+            return('<h1>The exchange rates between GBP and EUR in {} is {}!</h1>'.format(date,rates.eur))
+            return jsonify(rates)
     return('<h1>That date does not exist!</h1>')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
