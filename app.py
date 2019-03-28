03443b5ef5205346821302ec4222bc828e67b6ed
qmul-wyt <48768919+qmul-wyt@users.noreply.github.com>
2019-03-28 18:05:54 +0000

    Update app.py

--- app.py
+++ app.py
@@ -13,23 +13,19 @@ def hello():
 @app.route('/rates/<date>/<currency>')
 def profile(date,currency):
     rows = session.execute( """Select * From rates.stats where date = '{}'""".format(date))
-    if currency == 'cad':
+    if currency == 'cad':   #judge which currency we type in is
         for rates in rows:
-            #return('<h1>The exchange rates between GBP and CAD in {} is {}!</h1>'.format(date,rates.cad))
-            return jsonify(rates)
+            return jsonify(rates)   #return json format
     elif currency == 'usd':
         for rates in rows:
-            return('<h1>The exchange rates between GBP and USD in {} is {}!</h1>'.format(date,rates.usd))
             return jsonify(rates)
     elif currency == 'cny':
         for rates in rows:
-            return('<h1>The exchange rates between GBP and CNY in {} is {}!</h1>'.format(date,rates.cny))
             return jsonify(rates)
     elif currency == 'eur':
         for rates in rows:
-            return('<h1>The exchange rates between GBP and EUR in {} is {}!</h1>'.format(date,rates.eur))
             return jsonify(rates)
-    return('<h1>That date does not exist!</h1>')
+    return('<h1>That date or currency does not exist!</h1>')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
