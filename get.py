4fb12fe0e7dc79a7ca9b7899157b49f34c2c06ca
qmul-wyt <48768919+qmul-wyt@users.noreply.github.com>
2019-03-29 14:55:04 +0000

    Update get.py

--- get.py
+++ get.py
@@ -2,31 +2,27 @@ from flask import Flask, render_template, request, jsonify
 import json
 import requests_cache
 import requests
-from pprint import pprint
+from pprint import pprint    
 
 app = Flask(__name__)
 
 url_template = 'https://api.exchangeratesapi.io/history?base=GBP&symbols=CAD,USD,CNY,EUR&start_at={start}&end_at={end}'
-
-@app.route('/')
-def hello():
-    name = request.args.get("name","World")
-    return('<h1>Hello, {}!</h1>'.format(name))
+#use the exchage rate API but not completed
 
 @app.route('/rates', methods=['GET'])
 def rates():
     my_start = '2019-03-01'
-    my_end = '2019-03-26'
+    my_end = '2019-03-26'                #choose the the start and end time  
 
-    rates_url = url_template.format(start=my_start, end=my_end)
+    rates_url = url_template.format(start=my_start, end=my_end)             #the completed url we use
 
-    resp = requests.get(rates_url)
+    resp = requests.get(rates_url)   #use get method to download data
     if resp.ok:
         rates = resp.json()
         pprint(rates)
     else:
         print(resp.reason)
-    return jsonify(rates)
+    return jsonify(rates)     # return json file 
 
 if __name__=="__main__":
     app.run(port=8080, debug=True)
